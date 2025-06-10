b5d7cc555abe8f32e25f31940052590ffaddf9ca
theyamasakiman <74675760+theyamasakiman@users.noreply.github.com>
2025-06-09 23:12:00 -0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/theyamasakiman.github.io',
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
